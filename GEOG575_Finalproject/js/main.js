9a5349ec4df5fbb7c3c1db09391cb6ebbc6b4f0f
Kevin Palmer <rkpalmerjr@gmail.com>
2019-04-18 22:59:05 -0400

    Test Push
    Test Push

--- GEOG575_Finalproject/js/main.js
+++ GEOG575_Finalproject/js/main.js
@@ -1,3 +1,5 @@
+/*Bryan Garner, Sarah Grandstrand, Kevin Palmer, 2019*/
+
 $(document).ready(function () {
     //define baselayer tilesets
     var light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHNteXRoMiIsImEiOiJjaXNmNGV0bGcwMG56MnludnhyN3Y5OHN4In0.xsZgj8hsNPzjb91F31-rYA', {
